b3738dd205b26ade2d60f3099bc122ec50bffc41
kexy4958 <knighttkey@gmail.com>
2022-12-06 16:52:40 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import "./App.scss";
+import Card from "./Card";
+
+function App() {
+
+  return (
+    <div className="App">
+      <Card/>
+    </div>
+  )
+}
+
+export default App

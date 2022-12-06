caef708b95c95b0b42f5614ac8661dc575268872
kexy4958 <knighttkey@gmail.com>
2022-12-06 21:53:08 +0800

    update version

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build:{
-    outDir:'docs'   //輸出到docs
+  build: {
+    outDir: "docs", //輸出到docs
+    // emptyOutDir: true,
+
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split(".").at(1);
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = "img";
+          }
+          return `assets/${extType}/[name]-[hash][extname]`;
+        },
+
+        chunkFileNames: "assets/js/[name]-[hash].js",
+
+        entryFileNames: "assets/js/[name]-[hash].js",
+      },
+    },
   },
-  base: '/',  //資源路徑改為相對
+  base: "/", //資源路徑改為相對
   server: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     port: 9007,
   },
-})
+});
